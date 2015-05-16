990b20dd82b0559afd900823cf4d422c3ff572b2
prashantprakash <prashantpacy@gmail.com>
2015-05-16 06:13:41 -0500

    iterate

---
+++ iterate.py
@@ -0,0 +1,17 @@
+__author__ = 'PRASHANT'
+
+movies = ["Gladiator","Terminator","x-Men"]
+
+# using for to iterate over list
+
+for each_movie in movies:
+    print(each_movie)
+
+# using while loop to iterate over list
+
+count=0
+while(count< len(movies)):
+    print(movies[count])
+    count = count +1
+
+
