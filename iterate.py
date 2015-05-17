c96d5d0de18d8c652b77d290c6ccd19d0c6b0af6
prashantprakash <prashantpacy@gmail.com>
2015-05-17 12:55:29 -0500

    added more code

--- iterate.py
+++ iterate.py
@@ -1,6 +1,10 @@
 __author__ = 'PRASHANT'
 
-movies = ["Gladiator","Terminator","x-Men"]
+# movies = ["Gladiator","Terminator","x-Men"]
+movies = [
+"The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+["Graham Chapman",
+["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 
 # using for to iterate over list
 
@@ -13,5 +17,14 @@ count=0
 while(count< len(movies)):
     print(movies[count])
     count = count +1
+
+
+
+# using list inside list
+
+
+print(movies[4][1][3]) # will print Eric Idle
+
+
 
 
